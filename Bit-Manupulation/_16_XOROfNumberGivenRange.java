eda3d6d5cb9be75303e3629f6c06789b645028e8
ASHOK2706 <ashokkumar27b@gmail.com>
2025-02-28 18:50:08 +0530

    Bit-Manupulation

--- Bit-Manupulation/_16_XOROfNumberGivenRange.java
+++ Bit-Manupulation/_16_XOROfNumberGivenRange.java
@@ -4,6 +4,22 @@ Input: L = 4, R = 8
 Output: 8 
 Explanation: 4 ^ 5 ^ 6 ^ 7 ^ 8 = 8
 */
+
+import java.util.Scanner;
+
 public class _16_XOROfNumberGivenRange {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int L = in.nextInt();
+        int R = in.nextInt();
+        System.out.println(getXOR(L - 1) ^ getXOR(R));
+        in.close();
+    }
+
+    private static int getXOR(int n) {
+        if (n % 4 == 0) return n;
+        else if(n % 4 == 1) return 1;
+        else if(n % 4 == 2) return n+1;
+        else return 0;
+    }
 }
--- Bit-Manupulation/_17_DivideNumberWithoutUsingDivisionOperator.java
+++ Bit-Manupulation/_17_DivideNumberWithoutUsingDivisionOperator.java
@@ -10,6 +10,44 @@ Note: Assume we are dealing with an environment that could only store integers w
 32-bit signed integer range: [−231, 231 − 1]. For this problem, if the quotient is strictly greater than 231 - 1, 
 then return 231 - 1, and if the quotient is strictly less than -231, then return -231.
 */
+
+import java.util.Scanner;
+
 public class _17_DivideNumberWithoutUsingDivisionOperator {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int dividend = in.nextInt();
+        int dovisor = in.nextInt();
+        System.out.println(divide(dividend, dovisor));
+        in.close();
+    }
+
+    private static int divide(int dividend, int divisor) {
+        if(dividend == divisor) return 1;
+        boolean sign = true;
+        if(dividend >= 0 && divisor < 0) sign = false;
+        if(dividend < 0 && divisor > 0) sign = false;
+
+        long n = Math.abs(dividend);
+        long d = Math.abs(divisor);
+        long quiotent = 0;
+
+        while(n >= d) {
+            long count = 0;
+            while( n >= (d << (count + 1)) ) {
+                count++;
+            }
+            quiotent += 1 << count;
+            n -= d << count;
+        }
+        if(quiotent >= Integer.MAX_VALUE && sign) {
+            return Integer.MAX_VALUE;
+        }
+        if(quiotent >= Integer.MAX_VALUE && !sign) {
+            return Integer.MIN_VALUE;
+        }
+
+        return (sign == true) ? (int)quiotent : -1 * (int)quiotent;
+
+    }
 }
